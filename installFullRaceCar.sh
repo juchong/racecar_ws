8e65bfb0bbf3608e6c6c3c2b901c5ec28adb2514
Deva, Goutham <goutham.deva@analog.com>
2018-10-12 10:19:00 -0400

    added script

---
+++ installFullRaceCar.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Installing Racecar"
+
+cd ~
+mkdir -p racecar_ws/src
+cd racecar_ws/src
+
+# Clone all nodes used
+git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-usb_cam.git
+cd ADI-usb_cam
+./installUSBCamNode.sh
+cd ..
+git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-HLDS-lidar.git
+cd ADI-HLDS-lidar
+./instalLaserNode.sh
+cd ..
+git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-vesc.git
+git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-ToF.git
+git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-SLAM.git
+git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/racecar.git
+git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-Navigation.git
+
+cd ../..
+# cd /home/nvidia/racecar-ws
+catkin_make
+source devel/setup.bash
+# roslaunch ADI-ToF shubsub-test.launch
