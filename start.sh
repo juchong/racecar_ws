e75728c49a8ac883228d2843792833122c88fab3
Deva, Goutham <goutham.deva@analog.com>
2018-10-15 15:49:26 -0400

    Update quick install script

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 echo "Running Racecar"
+
+if [[ "$HOSTNAME" == "foobar" ]]; then
+  echo "Quick Install"
+  cd racecar_ws/src/ADI-usb_cam
+  ./installUSBCamNode.sh
+  cd ADI-HLDS-lidar
+  ./instalLaserNode.sh
+  cd ../../..
+fi
+
 cd racecar_ws
 catkin_make
 source devel/setup.bash
