2384084e6c9cae8b4b5275626f1d3dfe5d49a5c5
Deva, Goutham <goutham.deva@analog.com>
2018-10-15 13:20:41 -0400

    Added SubModules to Script

--- installFullRaceCar.sh
+++ installFullRaceCar.sh
@@ -6,19 +6,19 @@ mkdir -p racecar_ws/src
 cd racecar_ws/src
 
 # Clone all nodes used
-git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-usb_cam.git
+git submodule add git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-usb_cam.git
 cd ADI-usb_cam
 ./installUSBCamNode.sh
 cd ..
-git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-HLDS-lidar.git
+git submodule add git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-HLDS-lidar.git
 cd ADI-HLDS-lidar
 ./instalLaserNode.sh
 cd ..
-git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-vesc.git
-git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-ToF.git
-git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-SLAM.git
-git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/racecar.git
-git clone git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-Navigation.git
+git submodule add git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-vesc.git
+git submodule add git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-ToF.git
+git submodule add git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-SLAM.git
+git submodule add git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/racecar.git
+git submodule add git@gitlab.analog.com:ATS_Technology_Group/ROS_Projects/ADI-Navigation.git
 cd ../..
 
 # Stops running entire script after Control+C
