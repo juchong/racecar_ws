8a454b81b5c3b0ce217d3587d0429bfef8fb4687
Deva, Goutham <goutham.deva@analog.com>
2018-10-30 18:24:09 -0400

    organizing scripts in repo

---
+++ gitInstallScripts/boardSetup.sh
@@ -0,0 +1,26 @@
+cd .. 
+git branch
+git checkout master
+cd racecar_ws/src
+cd ADI-HLDS-lidar
+git init
+cd ..
+cd ADI-Navigation
+git init
+cd ..
+cd ADI-ToF
+git init
+cd ..
+cd ADI-usb_cam
+git init
+cd ..
+cd ADI-SLAM
+git init
+cd ..
+cd ADI-vesc
+git init
+cd ..
+cd racecar
+git init
+cd ../../..
+git submodule foreach git config receive.denyCurrentBranch 'warn'
