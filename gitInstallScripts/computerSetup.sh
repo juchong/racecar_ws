8b0963f258764e7c41d1ba3feca34f22a4e958f2
Deva, Goutham <goutham.deva@analog.com>
2018-11-08 10:07:37 -0500

    Update computerSetup.sh

--- gitInstallScripts/computerSetup.sh
+++ gitInstallScripts/computerSetup.sh
@@ -6,29 +6,32 @@ echo $IPnumber
 echo "Enter then name of the repo on your board"
 read racecar
 echo $racecar
+echo "Enter a name of your remote for all the submodules"
+read submodule
+echo $submodule
 cd racecar_ws/src/ADI-HLDS-lidar
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/ADI-HLDS-lidar
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_hldsLidar_kinetic
 cd ../ADI-Navigation
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/ADI-Navigation
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_navigation_kinetic
 cd ../ADI-SLAM
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/ADI-SLAM
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_SLAM_kinetic
 cd ../ADI-ToF
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/ADI-ToF
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_tof_kinetic
 cd ../ADI-usb_cam
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/ADI-usb_cam
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_usb_cam_kinetic
 cd ../ADI-IMU
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/ADI-IMU
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_IMU_kinetic
 cd ../ADI-vesc
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/ADI-vesc
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_vesc_kinetic
 cd ../racecar
-git remote rm rosnode
-git remote add rosnode ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/racecar
+git remote rm $submodule
+git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/racecar
 cd ../../..
-git submodule foreach git push rosnode master
+git submodule foreach git push $submodule master
