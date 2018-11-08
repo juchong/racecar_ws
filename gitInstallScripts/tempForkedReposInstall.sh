fbd93431b14ee21af9d0e50ed22f0cdf8f0b1e01
Deva, Goutham <goutham.deva@analog.com>
2018-11-08 10:17:07 -0500

    Update tempForkedReposInstall.sh

--- gitInstallScripts/tempForkedReposInstall.sh
+++ gitInstallScripts/tempForkedReposInstall.sh
@@ -8,7 +8,7 @@ rm -R launch
 cd ..
 mv hls_lfcd_lds_driver/* .
 rm -R -f hls_lfcd_lds_driver
-cd ../ADI-usb_cam
+cd ../adi_usb_cam_kinetic
 echo "Installing usb Cam Node Script"
 git clone https://github.com/ros-drivers/usb_cam.git
 cd usb_cam
@@ -17,9 +17,9 @@ cd ..
 mv usb_cam/* .
 rm -R -f usb_cam
 echo "Installing Analog Devices IMU Script"
-cd ../ADI-IMU
+cd ../adi_IMU_kinetic
 git clone https://github.com/tork-a/adi_driver.git
-mv ADI-IMU/* .
-rm -R -f ADI-IMU
+mv adi_driver/* .
+rm -R -f adi_driver
 echo "Install Complete"
 cd ../../..
