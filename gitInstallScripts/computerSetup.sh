5e9034500bfce3de3ce19b56e22b5d896fb6c130
Deva, Goutham <goutham.deva@analog.com>
2018-11-20 14:13:43 -0500

    Update computerSetup.sh

--- gitInstallScripts/computerSetup.sh
+++ gitInstallScripts/computerSetup.sh
@@ -10,25 +10,25 @@ echo "Enter a name of your remote for all the submodules"
 read submodule
 echo $submodule
 cd racecar_ws/src/adi_lidar_kinetic
-git remote rm $submodule
+#git remote rm $submodule
 git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_lidar_kinetic
 cd ../adi_slam_gmapping_kinetic
-git remote rm $submodule
+#git remote rm $submodule
 git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_slam_gmapping_kinetic
 cd ../adi_tof_kinetic
-git remote rm $submodule
+#git remote rm $submodule
 git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_tof_kinetic
 cd ../adi_usb_cam_kinetic
-git remote rm $submodule
+#git remote rm $submodule
 git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_usb_cam_kinetic
 cd ../adi_driver
-git remote rm $submodule
+#git remote rm $submodule
 git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_driver
 cd ../adi_vesc_kinetic
-git remote rm $submodule
+#git remote rm $submodule
 git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/adi_vesc_kinetic
 cd ../racecar
-git remote rm $submodule
+#git remote rm $submodule
 git remote add $submodule ssh://nvidia@$IPnumber/home/nvidia/$racecar/racecar_ws/src/racecar
 cd ../../..
 git submodule foreach git push $submodule master
