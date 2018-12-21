5798c2c4cb6e542c9619cd535880678c6d6b67b1
Deva, Goutham <goutham.deva@analog.com>
2018-12-21 09:53:21 -0500

    Update dependencies.sh

--- gitInstallScripts/dependencies.sh
+++ gitInstallScripts/dependencies.sh
@@ -29,5 +29,5 @@ echo "Lidar Permission for Sensors"
 sudo chmod a+rw /dev/ttyUSB0
 echo "USB and IMU dependencies"
 sudo apt-get -y install \
-    ros-kinetic-camera-info-manager ros-kinetic-image-transport libbullet-dev libsdl-image1.2-dev ros-kinetic-geometry2 ros-kinetic-navigation ros-kinetic-imu-filter-madgwick ros-kinetic-roslint 
+    ros-kinetic-camera-info-manager ros-kinetic-image-transport libbullet-dev libsdl-image1.2-dev ros-kinetic-geometry2 ros-kinetic-navigation ros-kinetic-imu-filter-madgwick ros-kinetic-roslint ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation ros-kinetic-interactive-markers 
 echo "Install Complete"
