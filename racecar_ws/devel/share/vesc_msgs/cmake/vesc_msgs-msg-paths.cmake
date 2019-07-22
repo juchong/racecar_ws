486b862d03a51dfbdd6df81a7f6ec9283ee7f84a
niall <niall.p.mullane@gmail.com>
2019-07-22 10:12:25 -0400

    removed jetson nano code and copied adi driver code from submodule

--- racecar_ws/devel/share/vesc_msgs/cmake/vesc_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(vesc_msgs_MSG_INCLUDE_DIRS "/home/analog/racecar_ws/racecar_ws/src/adi_vesc_kinetic/vesc_msgs/msg")
-set(vesc_msgs_MSG_DEPENDENCIES std_msgs)
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running Racecar"
-cd racecar_ws
-catkin_make
-source devel/setup.bash
-cd src
-roslaunch launch main.launch
